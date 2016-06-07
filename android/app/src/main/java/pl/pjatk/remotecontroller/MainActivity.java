65a623f2ff025c7c2920c84d222a4fa2d5f01f9e
Kamil Warpechowski <k.warpechowski@hakama.pl>
2016-06-08 01:03:19 +0200

    wstęp do styli

--- android/app/src/main/java/pl/pjatk/remotecontroller/MainActivity.java
+++ android/app/src/main/java/pl/pjatk/remotecontroller/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         startConnection(getBaseContext());
 
         this.bindButtons();
+        
     }
 
     private void bindButtons() {
