7466c9b8d37034701e637689bf6e0304738cdf28
Kamil Warpechowski <k.warpechowski@hakama.pl>
2016-06-10 01:20:02 +0200

    Custom button

---
+++ android/app/src/main/java/pl/pjatk/remotecontroller/CustomButton.java
@@ -0,0 +1,62 @@
+package pl.pjatk.remotecontroller;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kamilw on 09.06.2016.
+ */
+public class CustomButton extends Button {
+    private static ArrayList<CustomButton> buttons = new ArrayList<CustomButton>();
+    private TypedArray attributes;
+
+    public static void setLayout(int i) {
+
+        for (CustomButton btn : buttons) {
+            System.out.println("x");
+            btn.setBackgroundResource(i);
+        }
+    }
+
+    private void setUp() {
+        setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        buttons.add(this);
+        setText(attributes.getString(R.styleable.CustomButton_name));
+    }
+
+
+    public CustomButton(Context context) {
+        super(context, null);
+        setUp();
+    }
+
+    public CustomButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        attributes = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.CustomButton,
+                0, 0);
+
+        setUp();
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr, 0);
+        setUp();
+    }
+
+    public CustomButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setUp();
+    }
+
+
+
+}
--- android/app/src/main/java/pl/pjatk/remotecontroller/MainActivity.java
+++ android/app/src/main/java/pl/pjatk/remotecontroller/MainActivity.java
@@ -1,12 +1,15 @@
 package pl.pjatk.remotecontroller;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -22,65 +25,37 @@ import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.button1) Button btn1;
-    @Bind(R.id.button2) Button btn2;
-    @Bind(R.id.button3) Button btn3;
-    @Bind(R.id.button4) Button btn4;
-    @Bind(R.id.button5) Button btn5;
-    @Bind(R.id.button6) Button btn6;
-    @Bind(R.id.button7) Button btn7;
-    @Bind(R.id.button8) Button btn8;
-    @Bind(R.id.button9) Button btn9;
-    @Bind(R.id.button10) Button btn10;
-
-    private HashMap<Integer,Button> buttons = new HashMap<Integer,Button>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
         ButterKnife.bind(this);
         startConnection(getBaseContext());
 
-        this.bindButtons();
-        
+        click1();
     }
 
-    private void bindButtons() {
-        buttons.put(1, btn1);
-        buttons.put(2, btn2);
-        buttons.put(3, btn3);
-        buttons.put(4, btn4);
-        buttons.put(5, btn5);
-        buttons.put(6, btn6);
-        buttons.put(7, btn7);
-        buttons.put(8, btn8);
-        buttons.put(9, btn9);
-        buttons.put(10, btn10);
 
-        for(Map.Entry<Integer, Button> e : buttons.entrySet()) {
-            final Integer number = e.getKey();
-            e.getValue().setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    sendToServer(getString(R.string.btnCommand) + number);
-                }
-            });
-        }
-
+    //@OnClick(R.id.btn1)
+    protected void click1() {
+        System.out.println("click1");
+        this.changeLayout(R.drawable.light);
     }
 
-    private void disableButton(Integer buttonId) {
-        buttons.get(buttonId).setEnabled(false);
+   // @OnClick(R.id.btn2)
+    protected void click2() {
+        System.out.println("click2");
+        this.changeLayout(R.drawable.dark);
     }
 
-    private void enableButton(Integer buttonId) {
-        buttons.get(buttonId).setEnabled(true);
+
+    private void changeLayout(int myButton) {
+        CustomButton.setLayout(myButton);
     }
 
-    private void setText(Integer buttonId, String text) {
-        buttons.get(buttonId).setText(text);
-    }
+
+
+
 
     private void startConnection(Context context) {
         SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(context);
