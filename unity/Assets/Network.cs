18b0da8201072d83cb4cdf5522d13ec620a31d9d
Kamil Warpechowski <k.warpechowski@hakama.pl>
2016-06-12 13:59:14 +0200

    unity init

---
+++ unity/Assets/Network.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+
+	private SocketIOComponent socket;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("startuje");
+		GameObject go = GameObject.Find("SocketIO");
+		socket = go.GetComponent<SocketIOComponent>();
+
+		socket.On("open", TestOpen);
+		socket.On("button", Button);
+	}
+
+	public void TestOpen(SocketIOEvent e) {
+		socket.Emit ("register_game");
+	}
+
+	public void Button(SocketIOEvent e) {
+		Debug.Log ("xxxxX "+ e.data);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
