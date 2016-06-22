73e13e89d78ef1c861881c7eaab0de627fd14b98
Kamil Warpechowski <k.warpechowski@hakama.pl>
2016-06-22 14:45:01 +0200

    network2

--- unity/Assets/Network.cs
+++ unity/Assets/Network.cs
@@ -19,54 +19,28 @@ public class Network : MonoBehaviour {
     public void InitGame(SocketIOEvent e)
     {
         socket.Emit("register_game");
+        EnableButton(ButtonEnum.BUTTON1);
         SetText(ButtonEnum.BUTTON1, "Nazwa 1");
-        SetText(ButtonEnum.BUTTON2, "Nazwa 2");
-        SetText(ButtonEnum.BUTTON3, "Nazwa 3");
-        SetText(ButtonEnum.BUTTON4, "Nazwa 4");
-        SetText(ButtonEnum.BUTTON5, "Nazwa 5");
-        SetText(ButtonEnum.BUTTON6, "Nazwa 6");
+        DisableButton(ButtonEnum.BUTTON2);
     }
 
-    private void DisableButton(ButtonEnum btn) {
+    public void DisableButton(ButtonEnum btn) {
 		Dictionary<string, string> dic = new Dictionary<string, string> ();
 		dic.Add ("name", btn.ToString());
 		socket.Emit ("disable_button", new JSONObject (dic));
 	}
 
-	private void EnableButton(ButtonEnum btn) {
+	public void EnableButton(ButtonEnum btn) {
 		Dictionary<string, string> dic = new Dictionary<string, string> ();
 		dic.Add ("name", btn.ToString());
 		socket.Emit ("enable_button", new JSONObject (dic));
 	}
 
-	private void SetText(ButtonEnum btn, string text) {
+	public void SetText(ButtonEnum btn, string text) {
 		Dictionary<string, string> dic = new Dictionary<string, string> ();
 		dic.Add ("name", btn.ToString());
 		dic.Add ("text", text);
 		socket.Emit ("set_text", new JSONObject (dic));
-	}
-
-	IEnumerator RunPoints() {
-
-	
-
-		yield return new WaitForSeconds(5);
-		DisableButton (ButtonEnum.BUTTON1);
-		yield return new WaitForSeconds(5);
-		DisableButton (ButtonEnum.BUTTON2);
-		yield return new WaitForSeconds(5);
-		DisableButton (ButtonEnum.BUTTON3);
-		yield return new WaitForSeconds(5);
-		DisableButton (ButtonEnum.BUTTON4);
-		yield return new WaitForSeconds(5);
-		EnableButton (ButtonEnum.BUTTON1);
-		yield return new WaitForSeconds(5);
-		EnableButton (ButtonEnum.BUTTON2);
-		yield return new WaitForSeconds(5);
-		EnableButton (ButtonEnum.BUTTON3);
-		yield return new WaitForSeconds(5);
-		EnableButton (ButtonEnum.BUTTON4);
-
 	}
 
 
