2e23d28c7e6b64818df2e6c39849bcfc999a9cd2
Kamil Warpechowski <k.warpechowski@hakama.pl>
2016-06-24 20:59:11 +0200

    literówki

--- unity/Assets/ButtonEnum.cs
+++ unity/Assets/ButtonEnum.cs
@@ -2,9 +2,9 @@
 
 namespace AssemblyCSharp
 {
-	public enum ButtonEnum
-	{
-		BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6, BUTTON7, BUTTON8, BUTTON9, BUTTON10
-	}
+    public enum ButtonEnum
+    {
+        BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6, BUTTON7, BUTTON8, BUTTON9, BUTTON10
+    }
 }
 
